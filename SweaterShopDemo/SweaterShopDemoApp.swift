a2773d18d854ae66ff59470b0361269873537cdd
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-31 12:13:59 +0300

    Initial Commit

---
+++ SweaterShopDemo/SweaterShopDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopDemoApp.swift
+//  SweaterShopDemo
+//
+//  Created by Ömer Faruk Kılıçaslan on 31.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
